ec67c70bbda4f5fabcf944ed005bc3bda4406743
momodigital <momodigital@gmail.com>
2025-06-29 13:31:13 +0800

    Update Cgvindo.kt

--- Rebahin/src/main/kotlin/com/rebahin/Cgvindo.kt
+++ Rebahin/src/main/kotlin/com/rebahin/Cgvindo.kt
@@ -2,7 +2,7 @@ package com.rebahin
 
 class Cgvindo : Rebahin() {
 
-    // override var mainUrl = "http://62.171.170.1/"
+    // override var mainUrl = "https://cgvindo2.bond/"
     override var mainUrl = "https://cgvindo.christmas/"
     override var name = "Cgvindo"
 }
